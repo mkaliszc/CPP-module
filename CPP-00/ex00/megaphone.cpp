729678deda29289c45044866386846ad0b0bc6d6
Milan Kaliszczak <mkaliszc@c2r3p4.42lehavre.fr>
2025-02-11 21:26:11 +0100

    ex00

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/10 19:37:12 by mkaliszc          #+#    #+#             */
+/*   Updated: 2025/02/11 19:35:51 by mkaliszc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int	j = 0;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return(0);
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (char)std::toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return(0);
+}
